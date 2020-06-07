480380e3c956d270a48d81d1e6db59bd9301e4fe
avacallery <avacallery@gmail.com>
2020-06-07 14:48:01 -0400

    updated leapYear function

--- leapyear.js
+++ leapyear.js
@@ -1,11 +1,12 @@
 function calculateLeapYear(number) {
-  if (number % 4 === 0) {
-    return true;
-  } else if (number % 100 !== 0 && number % 400 === 0) {
+  if (number % 4 === 0 && (number % 100 !== 0 || number % 400 === 0)) {
     return true;
   } else {
     return false;
   }
 }
 
+console.log(calculateLeapYear(1900));
+console.log(calculateLeapYear(1600));
+console.log(calculateLeapYear(2020));
 console.log(calculateLeapYear(2024));
